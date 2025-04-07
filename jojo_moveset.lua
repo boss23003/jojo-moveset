dc8e2e10874d7ca62434563ddd395020b8a2ee3f
boss23003 <stenderdana@gmail.com>
2025-04-07 21:24:37 +0300

    jojo_moveset.lua

---
+++ jojo_moveset.lua
@@ -0,0 +1,5 @@
+-- JoJo Moveset for The Strongest Battlegrounds (Saitama Base)
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Reapvitalized/TSB/main/VEXOR.lua"))()
+
+-- Здесь будет твой полный кастом: анимации, эффекты, звуки и скиллы
